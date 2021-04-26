c98431e1fbb6637ceb25766dc516978064dd898c
Vivsvaan Sharma <sharmavivsvaan@gmail.com>
2021-04-26 19:48:46 +0530

    Update two_way_dict.py

--- Two_Way_Dictionary/two_way_dict.py
+++ Two_Way_Dictionary/two_way_dict.py
@@ -1,1 +1,18 @@
 # two_way_dict.py
+
+# can inherit from [InsensitiveDict]()
+# class TwoWayDict(InsensitiveDict):
+# in __init__, self.rev = InsensitiveDict()
+
+
+class TwoWayDict:
+    def __init__(self, mappings):
+        self.rev = {}
+        for key, val in mappings.items():
+            self[key] = val
+            self.rev[val] = key
+    
+ 
+
+        
+        
